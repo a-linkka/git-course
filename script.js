1d34c81ce83276124faaac343aa5eb1f22a50b2b
Alina Belova <twilight_2002@mail.ru>
2023-01-25 11:34:01 +0300

    упражнение функция разбора url

--- script.js
+++ script.js
@@ -40,25 +40,38 @@
 
 // console.log(pokupka(27, false, 1899))
 
-const tasks = ['Task 1', 'Task 2', 'Task 3', 'Task 4']
+// const tasks = ['Task 1', 'Task 2', 'Task 3', 'Task 4']
 
-function addTask(task){
-    tasks.push(task) 
-    return tasks 
-}
+// function addTask(task){
+//     tasks.push(task) 
+//     return tasks 
+// }
 
-function deleteTask(task){
-    tasks.splice(tasks.indexOf(task),1)
-    return tasks
-}
+// function deleteTask(task){
+//     tasks.splice(tasks.indexOf(task),1)
+//     return tasks
+// }
 
-function updateTask(task){
-    const oldTask = tasks[tasks.indexOf(task)]
-    tasks.splice(tasks.indexOf(task),1)
-    tasks.unshift(oldTask)
-    return tasks
-}
+// function updateTask(task){
+//     const oldTask = tasks[tasks.indexOf(task)]
+//     tasks.splice(tasks.indexOf(task),1)
+//     tasks.unshift(oldTask)
+//     return tasks
+// }
 
-console.log(addTask('Task 5'))
-console.log(deleteTask('Task 1'))
-console.log(updateTask('Task 5'))
+// console.log(addTask('Task 5'))
+// console.log(deleteTask('Task 1'))
+// console.log(updateTask('Task 5'))
+
+
+
+const url = 'https://purpleschool.ru/course/javascript'
+
+const url1 = url.split(':')
+console.log(url1[0])
+
+const url2 = url.split('/')
+console.log(url2[url2.indexOf('purpleschool.ru')])
+
+const url3 = url2.slice(url2.indexOf('purpleschool.ru') + 1, url2.length)
+console.log('/' + url3.join('/'))
