e3e9583e27e82c88cc7f1873cc5a0a1a2d84a7c7
Alina Belova <twilight_2002@mail.ru>
2023-01-25 10:38:32 +0300

    упражнение обновление списка задач

--- script.js
+++ script.js
@@ -1,41 +1,22 @@
-// const a = prompt('Сколько будет 7 + 15?');
-// console.log(a == 22 ? 'Успех':'Вы робот')
-// switch (true) {
-//     case a ==='Я не робот' :
-//     case Number(a) === 22:
-//         console.log('Успех');
-//         break;
-//     default:
-//         console.log('Вы робот!')
-//         break;
-// }
+const tasks = ['Task 1', 'Task 2', 'Task 3', 'Task 4']
 
-
-// const balance = 1002;
-// const bonusBalance = 111;
-// const isBanned = true;
-// const isExist = true;
-// const isSelling = true;
-// console.log(((balance > 1000 || bonusBalance > 100) && isBanned && isExist && isSelling) ? 'Можете приобрести игру':'Вы не можете приобрести игру' )
-
-const pokupka = (age, rabota, money) => {
-    if (money >= 2000){
-        return true
-    }
-    else if (money + functionMoney(age,rabota) >= 2000){
-        return true
-    }
-    return false
+function addTask(task){
+    tasks.push(task) 
+    return tasks 
 }
 
-function functionMoney(age,rabota){
-    if (age >= 24 && rabota){
-        return 500
-    }
-    else if (age >= 24) {
-        return 100
-    }
-    return 0
+function deleteTask(task){
+    tasks.splice(tasks.indexOf(task),1)
+    return tasks
 }
 
-console.log(pokupka(27, false, 1899))
+function updateTask(task){
+    const oldTask = tasks[tasks.indexOf(task)]
+    tasks.splice(tasks.indexOf(task),1)
+    tasks.unshift(oldTask)
+    return tasks
+}
+
+console.log(addTask('Task 5'))
+console.log(deleteTask('Task 1'))
+console.log(updateTask('Task 5'))
