20a69bb879ced793b266dc8712b9a84b8001e837
Alina Belova <twilight_2002@mail.ru>
2023-01-30 16:42:51 +0300

    DOM обработка нажатий

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const panelText = document.querySelector('.panel').innerText;
-console.log(panelText);
-document.querySelector('.panel').innerText = 'New text';
-document.querySelector('.input').value = 'Text';
+// const panelText = document.querySelector('.panel').innerText;
+// console.log(panelText);
+// document.querySelector('.panel').innerText = 'New text';
+// document.querySelector('.input').value = 'Text';
+
+// document.querySelector('.button').addEventListener('click', function(){
+//     console.log('Hi!')
+// })
+
+document.querySelector('.button').addEventListener('click', function(){
+    const inp = document.querySelector('.input').value;
+    if (!inp) {
+        return;
+    }
+    document.querySelector('.panel').innerText = inp;
+    document.querySelector('.input').value = '';
+    console.log(inp)
+})
