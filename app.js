c3f19ee8b9ee77f3a4e0d9cdceeb5b502cb45917
Alina Belova <twilight_2002@mail.ru>
2023-01-30 21:26:53 +0300

    DOM обработка нажатий клавиатуры

--- app.js
+++ app.js
@@ -7,7 +7,18 @@
 //     console.log('Hi!')
 // })
 
-document.querySelector('.button').addEventListener('click', function(){
+// document.querySelector('.button').addEventListener('click', function(){
+//     const inp = document.querySelector('.input').value;
+//     if (!inp) {
+//         return;
+//     }
+//     document.querySelector('.panel').innerText = inp;
+//     document.querySelector('.input').value = '';
+//     console.log(inp)
+// })
+
+
+function changeClick() {
     const inp = document.querySelector('.input').value;
     if (!inp) {
         return;
@@ -15,4 +26,19 @@ document.querySelector('.button').addEventListener('click', function(){
     document.querySelector('.panel').innerText = inp;
     document.querySelector('.input').value = '';
     console.log(inp)
-})
+}
+
+function changeInput(e) {
+    if (e.code == 'Enter') {
+        console.log('Enter')
+        changeClick();
+    }
+    
+}
+
+// document.querySelector('.input').addEventListener('keydown', function (e) {
+//     if (e.code == 'Enter') {
+//         console.log('Enter')
+//     }
+//     changeClick();
+// })
