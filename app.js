e510e8a50440305be9e6e1eedf017e04e7ab5be0
Alina Belova <twilight_2002@mail.ru>
2023-01-31 09:44:50 +0300

    Упражнение получение DOM элементов

--- app.js
+++ app.js
@@ -18,27 +18,43 @@
 // })
 
 
-function changeClick() {
-    const inp = document.querySelector('.input').value;
-    if (!inp) {
-        return;
-    }
-    document.querySelector('.panel').innerText = inp;
-    document.querySelector('.input').value = '';
-    console.log(inp)
-}
+// function changeClick() {
+//     const inp = document.querySelector('.input').value;
+//     if (!inp) {
+//         return;
+//     }
+//     document.querySelector('.panel').innerText = inp;
+//     document.querySelector('.input').value = '';
+//     document.querySelector('.notification').style.display = 'block';
+// }
 
-function changeInput(e) {
-    if (e.code == 'Enter') {
-        console.log('Enter')
-        changeClick();
-    }
+// function changeInput(e) {
+//     if (e.code == 'Enter') {
+//         console.log('Enter')
+//         changeClick();
+//     }
     
-}
+// }
 
 // document.querySelector('.input').addEventListener('keydown', function (e) {
 //     if (e.code == 'Enter') {
 //         console.log('Enter')
+//         changeClick();
 //     }
-//     changeClick();
 // })
+
+// const el1 = document.querySelector('.one').innerText
+// console.log(el1)
+// const el2 = document.querySelector('.one ~ div').innerText
+// console.log(el2)
+console.log(document.querySelectorAll('.one')[0].innerText);
+console.log(document.querySelectorAll('.one')[1].innerText);
+// const el3 = document.querySelector('#two').innerText
+// console.log(el3)
+console.log(document.getElementById('two').innerText)
+const el4 = document.querySelector('[user-id="4"]').innerText
+console.log(el4)
+
+
+
+
