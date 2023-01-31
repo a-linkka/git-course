060e84bf56cd31918d39725d68e18db0f36615df
Alina Belova <twilight_2002@mail.ru>
2023-01-31 11:39:46 +0300

    Упражнение JSON

--- app.js
+++ app.js
@@ -18,23 +18,27 @@
 // })
 
 
-// function changeClick() {
-//     const inp = document.querySelector('.input').value;
-//     if (!inp) {
-//         return;
-//     }
-//     document.querySelector('.panel').innerText = inp;
-//     document.querySelector('.input').value = '';
-//     document.querySelector('.notification').style.display = 'block';
-// }
+function changeClick() {
+    const inp = document.querySelector('.input').value;
+    if (!inp) {
+        return;
+    }
+    document.querySelector('.panel').innerText = inp;
+    document.querySelector('.input').value = '';
+    document.querySelector('.notification').style.display = 'block';
+    const textString = JSON.stringify({
+        text: inp
+    });
+    localStorage.setItem('text', textString)
+}
 
-// function changeInput(e) {
-//     if (e.code == 'Enter') {
-//         console.log('Enter')
-//         changeClick();
-//     }
+function changeInput(e) {
+    if (e.code == 'Enter') {
+        console.log('Enter')
+        changeClick();
+    }
     
-// }
+}
 
 // document.querySelector('.input').addEventListener('keydown', function (e) {
 //     if (e.code == 'Enter') {
@@ -65,10 +69,14 @@
 // document.querySelector('.test').appendChild(newEl);
 
 
-localStorage.setItem('token', '1111');
-localStorage.setItem('token1', 1);
-localStorage.setItem('token', true);
-const token1 = localStorage.getItem('token1');
-console.log(typeof token1);
-localStorage.removeItem('token1');
-localStorage.clear();
+// localStorage.setItem('token', '1111');
+// localStorage.setItem('token1', 1);
+// localStorage.setItem('token', true);
+// const token1 = localStorage.getItem('token1');
+// console.log(typeof token1);
+// localStorage.removeItem('token1');
+// localStorage.clear();
+
+// const obj = JSON.parse('{"a": 1}');
+// console.log(obj);
+
